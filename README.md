f9e4d2406703aae753f009e9f59f95a62d1ddf89
Tong <tong@disktree.net>
2016-03-25 20:39:03 +0100

    Remov src

--- README.md
+++
@@ -1,9 +0,0 @@
-
-# FUbar - 73L3vI5I0n
-
-Animated GIF Television Channel.
-
-
-![http://giphy.com/](data/fubar-icon-512.png)
-
-![http://giphy.com/](data/giphy-logo.png)
--- src/fubar/PlayMode.hx
+++
@@ -1,7 +0,0 @@
-package fubar;
-
-@:enum abstract PlayMode(String) from String to String {
-    var trending = "trending";
-    var search = "search";
-}
-//enum PlayMode
